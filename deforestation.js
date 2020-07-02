e03e3345163e6852c9fe68987dbe3b0c18a39743
kundusne000 <67029728+kundusne000@users.noreply.github.com>
2020-07-02 17:46:10 -0500

    Add files via upload

--- deforestation.js
+++ deforestation.js
@@ -1,12 +1,24 @@
-function slider() {
-    var s = document.getElementById("myRange").value;
-    if (s>"50" & s<"60"){
-        document.getElementById("paragraph").innerHTML = "Correct!";
+window.addEventListener("input", showResult, false)
+function showResult() {
+    var s = document.getElementById("rs-range-line").value;
+    if (s>40 & s<50){
+        document.getElementById("paragraph").innerHTML = "Correct! The exact number of acres that are cut down daily are 42 million acres. We can do so many things to redude this number if you just follow at least one of the five ways mentioned below. "; 
     }
-    else if (s<"50"){
-        document.getElementById("paragraph").innerHTML ="Close, however the number of acres cut down is more than that.";
+    else if (s<40){
+        document.getElementById("paragraph").innerHTML ="Close, however the number of acres cut down is more than that."; 
     }
     else {
-        document.getElementById("paragraph").innerHTML = "Close, however the number of acres cut down is less than that.";
+        document.getElementById("paragraph").innerHTML = "Close, however the number of acres cut down is less than that."; 
     }
+}
+
+
+window.addEventListener("input", showSliderValue, false)
+
+function showSliderValue() {
+  var rangeSlider = document.getElementById("rs-range-line");
+  var rangeBullet = document.getElementById("rs-bullet");
+  rangeBullet.innerHTML = rangeSlider.value;
+  var bulletPosition = (rangeSlider.value /rangeSlider.max);
+  rangeBullet.style.left = (bulletPosition * 578) + "px";
 }
